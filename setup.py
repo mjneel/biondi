4d4552c44de00e317cb7f1b69a7afa17b9d4e95b
Michael Neel <neelm@uci.edu>
2020-10-23 13:16:08 -0700

    Reorganized file structure and created README.md, LICENSE, setup.py, requirements.txt, and __init__.py files.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh
+    long_description = fh.read()
+
+setuptools.setup(
+    name='Biondi',
+    version='0.1',
+    author='Michael Neel',
+    author_email='neelm@uci.edu',
+    description='Machine learning code and resources for analyzing choroid plexus pathology',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+    url='https://github.com/mjneel/Biondi',
+    license='GNU GPLv3',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
